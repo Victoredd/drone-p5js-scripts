44ad3da1a6fbe3583bac597246ef0dc7e6636616
Víctor V <victorvillarejoalier@lamiranda.eu>
2025-03-31 11:00:07 +0200

    feat: add proportional control

--- drone-simulation.js
+++ drone-simulation.js
@@ -1,3 +1,5 @@
+gravity = 0;
+drag_coeff = 0.1;
 class Drone {
   constructor(x, y, vx, vy, mass, dt, controlStrategy) {
     this.position = createVector(x, y);
@@ -8,7 +10,7 @@ class Drone {
     this.dt = dt;
     this.mass = mass;
     this.controlStrategy = controlStrategy;
-    this.motorPower = 200 //the max amount of combined power the motors can output, in force units
+    this.motorPower = 900 //the max amount of combined power the motors can output, in force units
   }
   
   setPhysicalForces(fx, fy) {
@@ -16,8 +18,10 @@ class Drone {
   }
 
   setTotalForce() {
-    this.controlStrategy(0, 0);
+    this.cforce = p5.Vector.mult(this.controlStrategy(this.position.x, this.position.y), this.motorPower);
     this.force = p5.Vector.add(this.pforce, this.cforce);
+    //calculate air resistance
+      this.force.add(0, -drag_coeff*this.velocity.y);
   }
   
   update() {
@@ -32,32 +36,51 @@ class Drone {
   }
   
   display() {
-    translate(width/2, height/2);
     fill("red");
     rect(this.position.x - 100, this.position.y - 10, 200, 20);
   }
-  //control methods
+}
 
-  bangBangControl(obPosX, obPosY) {
-    
+//control methods
+function noControl(posX, posY, i, d) {
+  return createVector(0, 0);
+}
+
+function bangBangControl(posX, posY, i, d) {
+  if (posY > 0) {
+    return createVector(0, -1);
   }
-
-  noControl(obPosX, obPosY) {
-    this.cforce = createVector(0, 0);
+  else {
+    return createVector(0, 0);
   }
+}
 
+function bidirectionalBangBangControl(posX, posY, i, d) {
+  if (posY > 0) {
+    return createVector(0, -1);
+  }
+  else {
+    return createVector(0, 1);
+  }
+}
+
+function proportionalControl(posX, posY, p, i, d) {
+  return createVector(0, -2*posY/height);
 }
 
 function setup() {
   createCanvas(700, 700);
   frameRate(60);
-  drone = new Drone(0, 0, 0, 0, 1, 1/60, Drone.noControl);
-  drone.setPhysicalForces(0, 50);
+  drone = new Drone(0, 150, 0, 0, 1, 1/60, proportionalControl);
+  drone.setPhysicalForces(0, gravity);
 }
 
 
 function draw() {
+  translate(width/2, height/2);
   background(220);
   drone.update();
   drone.display();
+  fill("blue");
+  circle(0, 0, 10);
 }
